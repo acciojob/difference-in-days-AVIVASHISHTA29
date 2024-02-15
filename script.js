563ba217deb830bab579ac4312786bd0baf325ba
Avi Vashishta <avivashishta@gmail.com>
2024-02-15 17:10:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	let d1 = new Date(date1);
+	let d2 = new Date(date2);
+
+	let diffInMill = Math.floor((Date.UTC(d1.getFullYear(),d1.getMonth(),d1.getDate()) - Date.UTC(d2.getFullYear(),d2.getMonth(),d2.getDate()))/24*60*60*1000)
+	return diffInMill;
 };
 
 // Do not change the code below.
